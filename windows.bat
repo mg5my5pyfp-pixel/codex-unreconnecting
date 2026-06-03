a679ec4a1b5fc79b75bccda4515e3835abf69a95
mg5my5pyfp-pixel <mg5my5pyfp@privaterelay.appleid.com>
2026-06-03 13:18:44 +0800

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -6,4 +6,4 @@ set "WS_PROXY=http://127.0.0.1:7897"
 set "WSS_PROXY=http://127.0.0.1:7897"
 
 taskkill /F /IM Code.exe >nul 2>nul
-start "" "F:\Microsoft VS Code\Code.exe"
+start "" ""C:\Users\Lenovo\Downloads\VSCodeUserSetup-x64-1.119.0.exe""
